e98e818299dba04a393d2dc996bd6b3e0f53fabd
Administrator <Administrator@PC241>
2020-06-05 11:37:10 +0700

    Add project files.

---
+++ NguyenMinhHien_lab3/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using NguyenMinhHien_lab3.Models;
+using NguyenMinhHien_lab3.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenMinhHien_lab3.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbConext;
+        public CoursesController()
+        {
+            _dbConext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories= _dbConext.Categories.ToList()
+            };
+            return View();
+        }
+    }
+}
---
+++ NguyenMinhHien_lab3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenMinhHien_lab3.MvcApplication" Language="C#" %>
